371fc04f0dc8b5c45895c851693aa8e26d803861
SHArtyom <sartiom1@gmail.com>
2023-05-16 00:24:23 +0300

    Major update due to step 5 of the project

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -5,6 +5,7 @@ import psycopg2
 import datetime
 import validators
 import os
+import requests
 from dotenv import load_dotenv
 
 
@@ -74,7 +75,10 @@ def urls():
     if request.method == 'GET':
         conn = connect()
         with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute('SELECT * FROM urls ORDER BY id DESC')
+            cursor.execute('SELECT DISTINCT ON (urls.id) urls.id, name, \
+                           url_checks.created_at, status_code from urls \
+                           LEFT JOIN url_checks on urls.id=url_checks.url_id \
+                           ORDER BY urls.id DESC')
             url_list = cursor.fetchall()
         conn.close()
         return render_template('urls.html', url_list=url_list)
@@ -100,9 +104,18 @@ def url_details(id):
 def url_checks(id):
     conn = connect()
     with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+        cursor.execute('SELECT name FROM urls WHERE id=%s', (id,))
+        url = cursor.fetchone().name
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
+        flash('Произошла ошибка при проверке', 'danger')
+        return redirect(url_for('url_details', id=id))
+    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
         cursor.execute('INSERT INTO url_checks \
-                       (url_id, created_at) VALUES (%s, %s)',
-                       (id, datetime.datetime.now()))
+                       (url_id, status_code, created_at) VALUES (%s, %s, %s)',
+                       (id, response.status_code, datetime.datetime.now()))
     conn.commit()
     conn.close()
     flash('Страница успешно проверена', 'success')
