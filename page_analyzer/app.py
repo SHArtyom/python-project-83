bc4ddf834556a917ca8485f31d67bce4b54063e2
SHArtyom <sartiom1@gmail.com>
2023-05-19 23:27:28 +0300

    Code refactoring due to rev.1

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, flash, get_flashed_messages, \
     render_template, request, redirect, url_for
-from psycopg2.extras import NamedTupleCursor
 from datetime import datetime as date
 from dotenv import load_dotenv
-from bs4 import BeautifulSoup
-from urllib.parse import urlsplit, urlunsplit
-import psycopg2
-import validators
+from page_analyzer.db_connect import make_query_get, make_query_set
+from page_analyzer.url_tools import validate, normalize_url, get_check_data
 import os
-import requests
+
+MSG_SUCCESS = 'Страница успешно добавлена'
+MSG_EXISTS = 'Страница уже существует'
+MSG_ADDED = 'Страница успешно добавлена'
+MSG_ERROR = 'Произошла ошибка при проверке'
+MSG_CHECK = 'Страница успешно проверена'
 
 
 load_dotenv()
@@ -16,44 +18,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-DATABASE_URL = os.getenv('DATABASE_URL')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-
-def connect():
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-    except Exception:
-        print('Can`t establish connection to database')
-    return conn
-
-
-def validate(url):
-    errors = []
-    if not validators.url(url):
-        errors.append('Некорректный URL')
-    elif not url:
-        errors.append('URL обязателен')
-    elif len(url) > 255:
-        errors.append('URL превышает 255 символов')
-    return errors
-
-
-def normalize_url(input_url):
-    url_parts = list(urlsplit(input_url))
-    normalized = [url_parts[0], url_parts[1], '', '', '']
-    return urlunsplit(normalized)
-
-
-def parse_content(content):
-    soup = BeautifulSoup(content, 'html.parser')
-    h1_tag = soup.find('h1')
-    title_tag = soup.find('title')
-    description_tag = soup.find('meta', attrs={'name': 'description'})
-    h1 = h1_tag.text.strip() if h1_tag else ''
-    title = title_tag.text.strip() if title_tag else ''
-    description = description_tag['content'].strip() if description_tag else ''
-    return h1, title, description
 
 
 @app.route('/')
@@ -61,7 +26,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/urls', methods=['GET', 'POST'])
+@app.post('/urls')
 def urls():
     if request.method == 'POST':
         input_url = request.form.get('url')
@@ -73,72 +38,48 @@ def urls():
             return render_template('index.html', messages=messages,
                                    input_url=input_url), 422
         url = normalize_url(input_url)
-        conn = connect()
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute('SELECT * FROM urls WHERE name=(%s)', (url,))
-            existing_url = cursor.fetchone()
+        existing_url = make_query_get('SELECT * FROM urls WHERE name=(%s)', url)
         if existing_url:
-            flash('Страница уже существует', 'info')
+            flash(MSG_EXISTS, 'info')
             id = existing_url.id
             return redirect(url_for('url_details', id=id))
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute('INSERT INTO urls (name, created_at) \
-                           VALUES (%s, %s)', (url, date.now()))
-        conn.commit()
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute('SELECT * FROM urls WHERE name=(%s)', (url,))
-            id = cursor.fetchone().id
-        conn.close()
-        flash('Страница успешно добавлена', 'success')
+        make_query_set('INSERT INTO urls (name, created_at) VALUES (%s, %s)',
+                       url, date.now())
+        id = make_query_get('SELECT * FROM urls WHERE name=(%s)', url).id
+        flash(MSG_ADDED, 'success')
         return redirect(url_for('url_details', id=id))
-    if request.method == 'GET':
-        conn = connect()
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-            cursor.execute('SELECT DISTINCT ON (urls.id) urls.id, name, \
-                           url_checks.created_at, status_code from urls \
-                           LEFT JOIN url_checks on urls.id=url_checks.url_id \
-                           ORDER BY urls.id DESC')
-            url_list = cursor.fetchall()
-        conn.close()
-        return render_template('urls.html', url_list=url_list)
 
 
-@app.route('/urls/<int:id>', methods=['GET'])
+@app.get('/urls')
+def get_urls():
+    query = 'SELECT DISTINCT ON (urls.id) urls.id, name, \
+             url_checks.created_at, status_code from urls \
+             LEFT JOIN url_checks on urls.id=url_checks.url_id \
+             ORDER BY urls.id DESC'
+    url_list = make_query_get(query, fetch='all')
+    return render_template('urls.html', url_list=url_list)
+
+
+@app.get('/urls/<int:id>')
 def url_details(id: int):
     messages = get_flashed_messages(with_categories=True)
-    conn = connect()
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute('SELECT * FROM urls WHERE id=%s', (id,))
-        url = cursor.fetchone()
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute('SELECT * FROM url_checks WHERE url_id=(%s) \
-                       ORDER BY id DESC', (id,))
-        checks = cursor.fetchall()
-    conn.close()
+    url = make_query_get('SELECT * FROM urls WHERE id=%s', id)
+    checks = make_query_get("SELECT * FROM url_checks WHERE url_id=(%s) ORDER \
+                            BY id DESC", id, fetch='all')
     return render_template('url_details.html', url=url,
                            checks=checks, messages=messages)
 
 
-@app.route('/urls/<int:id>/checks', methods=['POST'])
+@app.post('/urls/<int:id>/checks')
 def url_checks(id: int):
-    conn = connect()
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute('SELECT name FROM urls WHERE id=%s', (id,))
-        url = cursor.fetchone().name
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.exceptions.RequestException:
-        flash('Произошла ошибка при проверке', 'danger')
+    url = make_query_get('SELECT name FROM urls WHERE id=%s', id).name
+    check_data = get_check_data(url)
+    if check_data == 1:
+        flash(MSG_ERROR, 'danger')
         return redirect(url_for('url_details', id=id))
-    h1, title, description = parse_content(response.text)
-    status_code, created_at = response.status_code, date.now()
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute('INSERT INTO url_checks (url_id, status_code, h1, \
-                       title, description, created_at) \
-                       VALUES (%s, %s, %s, %s, %s, %s)',
-                       (id, status_code, h1, title, description, created_at))
-    conn.commit()
-    conn.close()
-    flash('Страница успешно проверена', 'success')
+    make_query_set('INSERT INTO url_checks (url_id, status_code, h1, \
+                   title, description, created_at) \
+                   VALUES (%s, %s, %s, %s, %s, %s)',
+                   id, *check_data, date.now())
+    flash(MSG_CHECK, 'success')
     return redirect(url_for('url_details', id=id))
---
+++ page_analyzer/url_tools.py
@@ -0,0 +1,43 @@
+import validators
+import requests
+from urllib.parse import urlsplit, urlunsplit
+from bs4 import BeautifulSoup
+
+
+def validate(url):
+    errors = []
+    if not validators.url(url):
+        errors.append('Некорректный URL')
+    elif not url:
+        errors.append('URL обязателен')
+    elif len(url) > 255:
+        errors.append('URL превышает 255 символов')
+    return errors
+
+
+def normalize_url(input_url):
+    url_parts = list(urlsplit(input_url))
+    normalized = [url_parts[0], url_parts[1], '', '', '']
+    return urlunsplit(normalized)
+
+
+def parse_content(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    h1_tag = soup.find('h1')
+    title_tag = soup.find('title')
+    description_tag = soup.find('meta', attrs={'name': 'description'})
+    h1 = h1_tag.text.strip() if h1_tag else ''
+    title = title_tag.text.strip() if title_tag else ''
+    description = description_tag['content'].strip() if description_tag else ''
+    return h1, title, description
+
+
+def get_check_data(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
+        return 1
+    h1, title, description = parse_content(response.text)
+    status_code = response.status_code
+    return [status_code, h1, title, description]
